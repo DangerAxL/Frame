149466b48faf37166dccb2d590e97b88520695f5
DangerAxL <gareco1791@gmail.com>
2024-09-10 20:29:34 -0300

    Update configurar.php
    aclaración sobre que cambios realizar en ruta url

--- app/config/configurar.php
+++ app/config/configurar.php
@@ -9,6 +9,7 @@ define('DB_NAME', '_NOMBRE_BASE_DATOS');
 define('RUTA_APP',dirname(dirname(__FILE__)));
 
 //RUTA url Ejemplo: http://localhost/frame/
+//modificar _RUTA_URL por el nombre de la carpeta donde se va a alojar todo el sistema
 define('RUTA_URL','_RUTA_URL');
 
 define('NOMBRESITIO','_NOMBRE_SITIO_A_DESARROLLAR');
